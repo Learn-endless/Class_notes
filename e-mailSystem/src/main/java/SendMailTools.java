e8f50583b53b3715c0c26e2e8c57635c1c458d62
xwj <1983318249@qq.com>
2022-01-04 22:12:34 +0800

    QQ邮件群发系统注释添加

--- e-mailSystem/src/main/java/SendMailTools.java
+++ e-mailSystem/src/main/java/SendMailTools.java
@@ -1,23 +1,40 @@
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
-
 /**
- * 邮件工具类
+ * 自定义发送邮件工具类
  */
 public class SendMailTools {
+    /**
+     *
+     * @param toUser    收件邮箱
+     * @param fromUser  发件邮箱
+     * @param fromUserPassWord  发件邮箱的POP3/SMTP服务密码
+     * @param theme     邮件主题
+     * @param content   邮件内容
+     * @return    返回一个提示，是否发送成功（true/false）
+     */
     public static boolean sendMail(String toUser, String fromUser,
                                    String fromUserPassWord, String theme, String content){
+        //实例化邮件资源
         SimpleEmail email = new SimpleEmail();
-
-        email.setSslSmtpPort("465");    //qq：qq邮件服务器的端口号
+        //设置qq邮件服务器的端口号
+        email.setSslSmtpPort("465");
+        //使用QQ邮箱的固定写法
         email.setHostName("smtp.qq.com");
+        //设置发件人的账户和POP3/SMTP服务密码
         email.setAuthentication(fromUser, fromUserPassWord);
+        //编码方式
         email.setCharset("UTF-8");
         try {
+            //收件人
             email.addTo(toUser);
+            //发件人
             email.setFrom(fromUser);
+            //邮件主题
             email.setSubject(theme);
+            //邮件内容
             email.setMsg(content);
+            //发送
             email.send();
             return true;
         } catch (EmailException e) {
--- e-mailSystem/src/main/java/Test.java
+++ e-mailSystem/src/main/java/Test.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-
 /**
  * 使用自定义邮件发送工具类实现群发消息
  */
@@ -8,14 +7,12 @@ public class Test {
     public static void main(String[] args) {
         String toFrom = "1983318249@qq.com";         //发件人
         String toFromPassWord = "xkfyejbensopeecc";  //发件人邮箱POP3/SMTP服务密码
-        String theme = "这是一封测试邮件";             //邮件主题
-        String content = "hello word!~~~";              //邮件具体内容
+        String theme = "这是一封测试邮件";              //邮件主题
+        String content = "hello word!~~~";           //邮件具体内容
         try(
                 FileReader fs = new FileReader("toUser.txt");//字符流
-				
-                BufferedReader br = new BufferedReader(fs);  //字符装饰流
+                BufferedReader br = new BufferedReader(fs);           //字符装饰流
                 ){
-            
             String ret = null;
             //循环读取,每次读一行
             while((ret = br.readLine())!=null){
